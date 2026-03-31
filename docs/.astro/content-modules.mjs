9376e382ef8f2cfa0918e302f9028c273dfe78c6
lopushok9 <y.g.tsygankov@gmail.com>
2026-03-31 12:18:36 +0300

    docs3

--- docs/.astro/content-modules.mjs
+++ docs/.astro/content-modules.mjs
@@ -1,11 +1,11 @@
 
 export default new Map([
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/infrastructure/provider-gateway.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Finfrastructure%2Fprovider-gateway.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/infrastructure/mcp-server.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Finfrastructure%2Fmcp-server.mdx&astroContentModuleFlag=true")],
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/getting-started/quick-start.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fquick-start.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/wallet/architecture.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fwallet%2Farchitecture.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/wallet/solana.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fwallet%2Fsolana.mdx&astroContentModuleFlag=true")],
-["src/content/docs/wallet/capabilities.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fwallet%2Fcapabilities.mdx&astroContentModuleFlag=true")],
-["src/content/docs/wallet/bitcoin.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fwallet%2Fbitcoin.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/wallet/bitcoin.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fwallet%2Fbitcoin.mdx&astroContentModuleFlag=true")],
+["src/content/docs/wallet/capabilities.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fwallet%2Fcapabilities.mdx&astroContentModuleFlag=true")]]);
 		
